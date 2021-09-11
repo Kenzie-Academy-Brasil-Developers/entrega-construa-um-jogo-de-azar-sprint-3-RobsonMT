cd16c8041c57a01091bf496a073a7112e336fddd
RobsonMT <rbsndev3@gmail.com>
2021-09-11 19:10:03 -0300

    animaçoes

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+// const btn = document.querySelector('#play')
+// btn.addEventListener('click', start)
+
+const btn = () =>{
+    document.querySelector('#play')
+    addEventListener('click', start)
+}
+btn();
+    
 let image = "./image/";
 let imagesList = new Array();
 
 for(let i=0 ; i <= 8; i++) {
-    imagesList[i] = image + i + ".png";
+    imagesList[i] = `${image}${i}${".png"}`;
     new Image().src = imagesList[i];
 }
     
 let counter = 0;
 
-function slot(){
+function start(){
+    alerts.classList.contains('winner');
+    alerts.classList.remove('winner');
+    alerts.classList.contains('loser');
+    alerts.classList.remove('loser');
     let Random = setInterval(function(){
     counter++; 
 
@@ -29,16 +42,19 @@ function slot(){
         let resultOfThird = imagesList[third];
 
         let showResult = document.querySelector('#showResult')
-
+        const winner = "Parabéns, voce ganhou!";
+        const twoHits = "Quase.., não foi dessa vez!";
+        const loser = "Você perdeu, Tente outra vez!";
 
         if((resultOfFirst == resultOfThird) && (resultOfFirst == resultOfSecond)){  
-            showResult.innerHTML = "Você ganhou!"; 
+            showResult.innerHTML = `${winner}`;
+            `${winnerAlert()}`
 
             }else if((resultOfFirst == resultOfSecond) || (resultOfFirst == resultOfThird) || (resultOfThird == resultOfSecond)){ 
-                showResult.innerHTML = "Quase conseguiu, não foi dessa vez!"; 
-
+                showResult.innerHTML = `${twoHits}`;
             }else{ 
-                showResult.innerHTML = "Você perdeu. Tente outra vez!"; 
+                showResult.innerHTML = `${loser}`; 
+                `${loserAlert()}`
             } 
             counter = 0; 
             clearInterval(Random); 
@@ -48,3 +64,15 @@ function slot(){
     },120);
           
 }
+
+const alerts = document.querySelector('.wrapper');
+
+function winnerAlert (){
+    alerts.classList.add('winner');
+}
+function loserAlert (){
+    alerts.classList.add('loser');
+}
+
+
+
