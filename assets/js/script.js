76fbbfbc2dfe0ec73ee2fdbe9309edc589408a71
RobsonMT <rbsndev3@gmail.com>
2021-09-11 22:12:49 -0300

    add/alertas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,44 @@
-// const btn = document.querySelector('#play')
-// btn.addEventListener('click', start)
-
 const btn = () =>{
     document.querySelector('#play')
     addEventListener('click', start)
 }
 btn();
     
-let image = "./image/";
+const alerts = document.querySelector('.wrapper');
+const alertsH = document.querySelector('.header');
+
+const playSoundAlert = document.querySelector('#playSound')
+const winnerSoundAlert = document.querySelector('#winnerSound')
+const loserSoundAlert = document.querySelector('#loserSound')
+
+function winnerAlert (){
+    alerts.classList.add('winner');
+    alertsH.classList.add('winner');
+}
+
+function TwoHits (){
+    alerts.classList.add('two-Hits');
+    alertsH.classList.add('two-Hits');
+}
+
+function loserAlert (){
+    alerts.classList.add('loser');
+    alertsH.classList.add('loser');
+}
+
+function winnerSound (){
+    winnerSoundAlert.play();
+}
+
+function playSound (){
+    playSoundAlert.play();
+}
+
+function loserSound (){
+    loserSoundAlert.play();
+}
+
+let image = "./assets/image/";
 let imagesList = new Array();
 
 for(let i=0 ; i <= 8; i++) {
@@ -18,10 +49,21 @@ for(let i=0 ; i <= 8; i++) {
 let counter = 0;
 
 function start(){
+
+    `${playSound()}`;
     alerts.classList.contains('winner');
     alerts.classList.remove('winner');
     alerts.classList.contains('loser');
     alerts.classList.remove('loser');
+    alertsH.classList.contains('winner');
+    alertsH.classList.remove('winner');
+    alertsH.classList.contains('loser');
+    alertsH.classList.remove('loser');
+    alerts.classList.contains('two-Hits');
+    alerts.classList.remove('two-Hits');
+    alertsH.classList.contains('two-Hits');
+    alertsH.classList.remove('two-Hits');
+
     let Random = setInterval(function(){
     counter++; 
 
@@ -36,7 +78,7 @@ function start(){
     document.second.src = imagesList[second]; 
     document.third.src = imagesList[third];
     
-    if (counter > 5){
+    if (counter > 10){
         let resultOfFirst = imagesList[first]; 
         let resultOfSecond = imagesList[second]; 
         let resultOfThird = imagesList[third];
@@ -48,31 +90,23 @@ function start(){
 
         if((resultOfFirst == resultOfThird) && (resultOfFirst == resultOfSecond)){  
             showResult.innerHTML = `${winner}`;
-            `${winnerAlert()}`
+            `${winnerAlert()}`;
+            `${winnerSound()}`;
 
             }else if((resultOfFirst == resultOfSecond) || (resultOfFirst == resultOfThird) || (resultOfThird == resultOfSecond)){ 
                 showResult.innerHTML = `${twoHits}`;
+                `${TwoHits()}`;
             }else{ 
                 showResult.innerHTML = `${loser}`; 
-                `${loserAlert()}`
+                `${loserSound()}`;
+                `${loserAlert()}`;
             } 
             counter = 0; 
             clearInterval(Random); 
 
         }
 
-    },120);
+    },250);
           
 }
-
-const alerts = document.querySelector('.wrapper');
-
-function winnerAlert (){
-    alerts.classList.add('winner');
-}
-function loserAlert (){
-    alerts.classList.add('loser');
-}
-
-
 
